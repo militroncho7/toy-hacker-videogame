3b5eec3f63f9f2b6b8c53d6165ac44b0958cf471
militroncho7 <victor@MacBook-Air-de-Victor.local>
2019-12-18 19:43:05 +0100

    Add Player

---
+++ js/player.js
@@ -0,0 +1,101 @@
+class Player {
+  constructor(ctx, width, height, image, gameWidth, gameHeight, keys) {
+    this.ctx = ctx;
+    this.width = width;
+    this.height = height;
+
+    this.image = new Image();
+    this.image.src = image;
+
+    this.posX = 50;
+    this.posY = 0;
+    this.posY0 = 600;
+
+    //this.posY = gameHeight * 0.98 - this.height ;
+    //this.posY0 = gameHeight * 0.98 - this.height ;
+
+    this.vy = 1;
+    //this.gravity = 0.4;
+    this.gameWidth = gameWidth;
+
+    this.frames = 3;
+    this.framesIndex = 0;
+
+    this.keys = keys;
+    this.bullets = [];
+    this.setListeners()
+  }
+
+  draw(framesCounter) {
+    this.ctx.drawImage(
+      this.image,
+      this.framesIndex * Math.floor(this.image.width / this.frames),
+      0,
+      Math.floor(this.image.width / this.frames),
+      this.image.height,
+      this.posX,
+      this.posY,
+      this.width,
+      this.height
+    )
+    // this.clearBullets()
+    // this.bullets.forEach(bullet => bullet.draw())
+    this.animate(framesCounter)
+  }
+
+  move() {
+    if (this.posY <= this.posY0 && this.posY >= 0) {
+      this.posY += this.vy;
+      // this.vy += this.gravity;
+    } else {
+      this.vy = 1;
+      if (this.posY >= 300) {
+        this.posY = this.posY0;
+      } else {
+        this.posY = 0;
+      }
+
+
+    }
+    //this.bullets.forEach(bullet => bullet.move())
+  }
+
+  animate(framesCounter) {
+    if (framesCounter % 10 === 0) {
+      this.framesIndex++;
+
+      if (this.framesIndex > 2) this.framesIndex = 0;
+    }
+  }
+
+
+
+  setListeners() {
+    document.addEventListener('keydown', (e) => {
+      switch (e.keyCode) {
+        case this.keys.TOP_KEY:
+          if (this.posY >= 0) {
+            this.posY -= this.vy;
+            this.vy -= 10;
+          }
+          break;
+
+        case this.keys.BOTTOM_KEY:
+          if (this.posY <= this.posY0) {
+            this.posY -= this.vy;
+            this.vy += 10;
+          };
+        // case this.keys.SPACE:
+        //   this.shoot()
+      }
+    })
+  }
+
+  shoot() {
+    this.bullets.push(new Bullet(this.ctx, 10, this.posX, this.posY, this.width, this.height, this.posY0))
+  }
+
+  clearBullets() {
+    this.bullets = this.bullets.filter(bullet => bullet.posX <= this.gameWidth)
+  }
+}
