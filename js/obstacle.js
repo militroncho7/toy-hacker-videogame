1c9489a66f67bd27c8ed9e6618a8d90c58f03fd3
militroncho7 <victor@MacBook-Air-de-Victor.local>
2019-12-18 19:44:24 +0100

    Add obstacle

---
+++ js/obstacle.js
@@ -0,0 +1,24 @@
+class Obstacle {
+  constructor(ctx, width, height, gameWidth, gameHeight, image) {
+    this.ctx = ctx;
+    this.width = 170;
+    this.height = 150;
+
+    this.image = new Image();
+    this.image.src = image;
+
+    this.posX = gameWidth;
+    this.posY = gameHeight * 0.98 - this.height;
+
+    this.vx = 10;
+  }
+
+  draw() {
+    this.ctx.drawImage(this.image, this.posX, this.posY, this.width, this.height);
+    //this.ctx.drawImage(this.image, this.posX + this.width, this.posY, this.width, this.height);
+  }
+
+  move() {
+    this.posX -= this.vx;
+  }
+}
